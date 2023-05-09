ce385cf7f43dcce8032a6396084ae8e0cd91b7c5
Jack-Maos <124993758+Jack-Maos@users.noreply.github.com>
2023-05-10 00:45:13 +0800

    Update my.js

--- pages/my/my.js
+++ pages/my/my.js
@@ -8,9 +8,9 @@ Page({
     //个人信息
     myInfo: {
       avatar: 'https://profile-avatar.csdnimg.cn/abcd29a7d2594dc4977563b7c362cc30_qq_54147913.jpg!1',//我csdn头像，带域名地址的
-      name: '毛佳骏 吴昊天',
-      classes: '软件工程 人工智能',
-      number: '20009200960 20009200671',
+      name: '***',
+      classes: '***',
+      number: '***',
     }
   },
 
